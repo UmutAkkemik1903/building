2391c70c6f22bd647209552ec6e4a771751f23d1
UmutAkkemik1903 <umutakkemik@gmail.com>
2024-03-15 12:37:27 +0300

    Migrationlar ayarlandı.

---
+++ backend/database/migrations/2024_03_15_085413_create_contact_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contact', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('email');
+            $table->string('fax');
+            $table->string('phone');
+            $table->string('address');
+            $table->string('google_map');
+            $table->string('created_by')->nullable();
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+            $table->timestamp('deleted_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contact');
+    }
+};
